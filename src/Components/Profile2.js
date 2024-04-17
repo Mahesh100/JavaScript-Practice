c30b0cc836990cdfca6315c281e1ef2e316e3edf
Mahesh Kakde <maheshkakde100@gmail.com>
2024-04-17 23:19:21 +0530

    added

---
+++ src/Components/Profile2.js
@@ -0,0 +1,12 @@
+import React  from "react";
+
+
+export default function Profile2(){
+    return
+    {
+        <div> 
+            <h1>This is Functional Component</h1></div>
+    }
+}
+
+
---
+++ src/variables.js
@@ -0,0 +1,22 @@
+// let, const and var
+
+//let
+
+// let variable is used only in block of scope in which it is define
+
+let message;  //declear
+
+//message = "Hey, how are you?";
+console.log(message);
+
+
+var a = 45;
+console.log(a);
+
+
+const b = "mahesh";
+const b = 2;
+console.log(b);
+
+
+
